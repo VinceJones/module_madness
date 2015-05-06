e3aff9310583b99ce1f31374d8beb57d4da58cad
lukepoppe <lukepoppe@hotmail.com>
2015-05-06 15:27:28 -0500

    port -> PORT

--- public/assets/app.js
+++ public/assets/app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 //var wrangler = require('./wrangler');
 
-app.set('port', (process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response){
     response.send('Hello World!');
 });
 
-//app.listen(app.get('port'), function(){
-//    console.log("Node is running on port: ", app.get('port'));
-//});
+app.listen(app.get('port'), function(){
+    console.log("Node is running on port: ", app.get('port'));
+});
