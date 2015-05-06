051918f32169947fda88607b3d50d741fc796aab
lukepoppe <lukepoppe@hotmail.com>
2015-05-06 15:22:05 -0500

    port -> PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 //var wrangler = require('./wrangler');
 
-app.set('port', (process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response){
     response.send('Hello World!');
